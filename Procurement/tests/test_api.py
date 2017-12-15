9a59f57fdb247cb8cf057a11e3fc3466866af536
Spground <958340585@qq.com>
2017-12-15 18:42:42 +0800

    Dev wujie 20171215 1 (#71)
    * 修改了README
    
    * 添加按主工作令uid模糊查询 子工作令 api
    
    * 编写采购单和采购物料的API文档
    
    * typo
    
    * pass flake8
    
    * fine
    
    * fine

---
+++ Procurement/tests/test_api.py
@@ -0,0 +1,9 @@
+from rest_framework.test import APITestCase
+
+
+class SubWorkOrderAPITest(APITestCase):
+    def test_subworkorder_query_by_workorder_uid(self):
+        """
+        测试子工作令根据主工作令UID模糊查询
+        """
+        pass
